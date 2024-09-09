f55ae560bb7183f73c6da75af67b72ae07eef3bb
David Parry <david.parry@suranyami.com>
2024-09-09 21:09:53 +1000

    Switch repository to git@github.com:suranyami/get-started.git

--- update-git-url.sh
+++
@@ -1,7 +0,0 @@
-ORIGINAL_URL="git@github.com:suranyami/get-started.git"
-NEW_URL=`git remote get-url origin`
-# NOTE: sed -i '' is required on MacOS but breaks on Linux, on Linux use -i'' without space instead
-grep -rl $ORIGINAL_URL | grep -v README.md | xargs sed -i '' "s|$ORIGINAL_URL|$NEW_URL|g"
-git add .
-git commit -m "Switch repository to $NEW_URL"
-git push
